edffe0d714a0c688f7be8905d1fd6b7d638053ef
Emildawood123 <edawood997@gmail.com>
2024-04-03 06:44:29 -0400

    forget add property

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -9,7 +9,7 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         """__init__ constructor"""
         super().__init__()
-        self._last = None
+        self._last_key = None
 
     def put(self, key, item):
         """put method"""
@@ -20,11 +20,11 @@ class LIFOCache(BaseCaching):
                 len(self.cache_data) == self.MAX_ITEMS
                 and key not in self.cache_data.keys()
             ):
-                discard = self._last
+                discard = self._last_key
                 del self.cache_data[discard]
                 print(f"DISCARD: {discard}")
             self.cache_data[key] = item
-            self._last = key
+            self._last_key = key
 
     def get(self, key):
         """get method"""
