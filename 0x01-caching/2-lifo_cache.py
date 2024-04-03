b87de195ad645059933bc0212819c2484f970f7e
Emildawood123 <edawood997@gmail.com>
2024-04-03 06:50:03 -0400

    forget add property

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -14,7 +14,7 @@ class LIFOCache(BaseCaching):
         self._last_key = None
 
     def put(self, key, item):
-        """Add an item in the cache"""
+        """put method"""
         if key is not None and item is not None:
             if (
                 len(self.cache_data) == self.MAX_ITEMS
@@ -28,7 +28,7 @@ class LIFOCache(BaseCaching):
             self._last_key = key
 
     def get(self, key):
-        """Get an item by key"""
+        """get method"""
         try:
             return self.cache_data[key]
         except Exception:
