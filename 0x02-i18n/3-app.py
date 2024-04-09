8a1442b096554017e0f9224b6cb49e593755e8a0
Emildawood123 <edawood997@gmail.com>
2024-04-09 09:29:04 -0400

    forget add property

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""3-app.py"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config(object):
+    """Config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """get_locale method"""
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route("/")
+def hello():
+    """hello method"""
+    return render_template("3-index.html")
