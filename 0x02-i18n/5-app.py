d7076b211663da4a112d47273247e2530e986669
Emildawood123 <edawood997@gmail.com>
2024-04-09 12:58:49 -0400

    forget add property

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""3-app.py"""
+
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config(object):
+    """Config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """get_user method"""
+    login_as = request.args.get('login_as')
+    if login_as and int(login_as) in users:
+        return users[int(login_as)]
+    else:
+        return None
+
+
+@app.before_request
+def before_request():
+    """before_request"""
+    print(get_user())
+    g.user = get_user()
+
+
+@app.route("/")
+def hello():
+    """hello method"""
+    username = None
+    login_as = request.args.get('login_as')
+    if login_as and int(login_as) in users:
+        username = g.user["name"]
+    return render_template("5-index.html", username=username)
+
+
+if __name__ == '__main__':
+    """main"""
+    app.run(debug=True)
